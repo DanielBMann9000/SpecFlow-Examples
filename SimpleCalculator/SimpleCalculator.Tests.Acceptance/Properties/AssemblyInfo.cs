069f076bd418fb27334ec7bef759df999534f902
Daniel Mann <daniel.mann@incyclesoftware.com>
2017-05-09 11:01:47 -0400

    added calculator project

---
+++ SimpleCalculator/SimpleCalculator.Tests.Acceptance/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleCalculator.Tests.Acceptance")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleCalculator.Tests.Acceptance")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4c93878-c7d7-4a8b-9079-6fc98d63ab62")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleCalculator/SimpleCalculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public static class Calculator
+    {
+        public static int Add(IEnumerable<int> numbers)
+        {
+            return numbers.Sum();
+        }
+
+        public static int Subtract(IEnumerable<int> numbers)
+        {
+            int acc = 0;
+            numbers.Select(s => acc - s).ToList();
+            return acc;
+        }
+    }
+}
